d9d33fa51160c040d061042dae12849b6f56396c
NaHyun0130 <hyun0130@gmail.com>
2017-11-11 12:38:49 +0900

    implement revise some code...

--- src/commands.c
+++ src/commands.c
@@ -55,7 +55,7 @@ int evaluate_command(int n_commands, struct single_command (*commands)[512])
 	 if(com->argv[0][0] == '/'){
 		pid_t pid;
 		pid = fork();
-		const char *args[] = {"/bin/ls", "-l", "ls", NULL};
+		char *args[] = {"/bin/ls", "-l", "ls", NULL};
 		int r;
 
 		if(pid < 0){
@@ -65,7 +65,7 @@ int evaluate_command(int n_commands, struct single_command (*commands)[512])
 			wait(NULL);
 		}
 		else if(pid == 0){
-			execv("/bin/ls", args);
+			r = execv(ars[0], args);
 			if(r == -1)
 			{
 				perror("execv");
